b1574f38e76cc9325ff7cd17f145775a5c6a734d
Taufan Arif <hadikusumaa15@gmail.com>
2018-02-21 13:34:22 +0700

    adding new mmigration

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Http\Request;
 
 Route::get('items', function (){
  Item::all();
- return ['nama : apel', 'harga : 1000','deskripsi : buah ini mengandung vitamin A', 'stok : 10 buah',' ', 'nama : jeruk', 'harga : 2000','deskripsi : buah ini mengandung vitamin C', 'stok : 11 buah',' ','nama : mangga', 'harga : 3000','deskripsi : buah ini mengandung vitamin C', 'stok : 12 buah'];
+ return ['nama : apelzzz', 'harga : 1000','deskripsi : buah ini mengandung vitamin A', 'stok : 10 buah',' ', 'nama : jeruk', 'harga : 2000','deskripsi : buah ini mengandung vitamin C', 'stok : 11 buah',' ','nama : mangga', 'harga : 3000','deskripsi : buah ini mengandung vitamin C', 'stok : 12 buah'];
 });
