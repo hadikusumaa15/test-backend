34f31f86585ce636a3f6a7dccbd9c7227285265a
Taufan Arif <hadikusumaa15@gmail.com>
2018-02-21 14:09:01 +0700

    abc

---
+++ routes/mencoba.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Item;
+use Illuminate\Http\Request;
+
+Route::get('items', function (){
+ Item::all();
+ return ['nama : apel', 'harga : 1000','deskripsi : buah ini mengandung vitamin A', 'stok : 10 buah',' ', 'nama : jeruk', 'harga : 2000','deskripsi : buah ini mengandung vitamin C', 'stok : 11 buah',' ','nama : mangga', 'harga : 3000','deskripsi : buah ini mengandung vitamin C', 'stok : 12 buah'];
+});
